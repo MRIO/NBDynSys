e21515e7816a8462e40a2175ecf9f3b76e4ba763
Romano van Genderen <romanovangenderen@protonmail.com>
2019-09-08 20:29:57 +0200

    Unit tests written for bifurcationdiagram

--- unittests/session_2/bifurcationdiagram.m
+++ unittests/session_2/bifurcationdiagram.m
@@ -1,8 +1,19 @@
-function [] = bifurcationdiagram() 
-  as = 0:0.01:4;
+function fighandle = bifurcationdiagram(as, varargin)	
+  % :param as: Array of range of parameters
+  % :param vararg: Variable argument to handle figure handle
+  % :returns: Figure handle containing the figure of a bifurcation diagram of the logistic map
+
+  if not(isempty(varargin))
+    if ishandle(varargin{1})
+      fighandle = varargin{1};
+    end
+  else
+    fighandle = figure;
+  end
+
+  ax = subplot(2, 1, 1);
   my_fixedpoints = arrayfun(@fixedpoints, as, 'UniformOutput', false);
   for i=1:numel(my_fixedpoints)
-    i
     for j=1:numel(my_fixedpoints{i})
       hold on
       scatter(as(i), my_fixedpoints{i}(j), 1, 'k');
@@ -10,6 +21,7 @@ function [] = bifurcationdiagram()
         break
     end
   end
+  hold off
 end
   
 
